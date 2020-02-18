3310eae2f848a34b340380061d2a840f465d0200
Noah Rosamilia <ivoahivoah@gmail.com>
2020-02-17 22:56:20 -0500

    Split long lines

--- Board.py
+++ Board.py
@@ -12,24 +12,33 @@ class Board:
             self.board[place[0]][place[1]] = random.choice(self.validMoves(*place))
     
     def getRow(self, row):
-        return set(self.board[row]) - {' '}
+        return self.board[row]
     
     def getColumn(self, column):
-        return {self.board[row][column] for row in range(9)} - {' '}
+        return [self.board[row][column] for row in range(9)]
     
     def getBox(self, row, column):
         row = row//3*3
         column = column//3*3
-        return {self.board[r][c] for r in range(row, row + 3) for c in range(column, column + 3)} - {' '}
+        return [self.board[r][c] for r in range(row, row+3) for c in range(column, column+3)]
 
     def validMoves(self, row, column):
-        return list({1, 2, 3, 4, 5, 6, 7, 8, 9} - self.getRow(row) - self.getColumn(column) - self.getBox(row, column))
+        return list(
+            {1, 2, 3, 4, 5, 6, 7, 8, 9}
+            - set(self.getRow(row))
+            - set(self.getColumn(column))
+            - set(self.getBox(row, column))
+            - {' '})
 
     def printBoard(self):
         print('┏━━━┯━━━┯━━━┳━━━┯━━━┯━━━┳━━━┯━━━┯━━━┓')
         for row in range(9):
-            print('┃' + '┃'.join('│'.join([f' {GREEN}{v}{RESET} ' if (row, i+j) in self.start else f' {v} ' for j, v in enumerate(self.board[row][i:i+3])]) for i in range(0, 9, 3)) + '┃')
-            if (row + 1)%3 == 0:
+            print('┃' + '┃'.join(
+                '│'.join(
+                    f' {GREEN}{v}{RESET} ' if (row, i+j) in self.start else f' {v} '
+                    for j, v in enumerate(self.board[row][i:i+3]))
+                for i in range(0, 9, 3)) + '┃')
+            if (row+1)%3 == 0:
                 if row != 8:
                     print('┣━━━┿━━━┿━━━╋━━━┿━━━┿━━━╋━━━┿━━━┿━━━┫')
             else:
